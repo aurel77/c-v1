bd28d43903155f61cd8d17039a9fd365269bb45a
root <root@localhost.localdomain>
2019-07-31 11:55:16 +0300

    initial comit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a =1;
+            var b =2;
+            var c =3;
+            System.Console.WriteLine(c==a && c==b);
+
+        }
+    }
+}
